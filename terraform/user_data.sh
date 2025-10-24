061b65033a11e9722a899177a506eb06dd203bc2
JoseManuel134975 <josemanuel3606@gmail.com>
2025-10-24 11:52:32 +0200

    user_data.sh add -> Archivos descargados desde repositorio >> Dockerfile change -> VOLUME-- COPY++

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Instalación Docker
 sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -9,10 +10,33 @@ sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Clona el repositorio
+sudo apt install git -y
+sudo git clone https://github.com/MrJosem/RA2-Tarea-repaso.git /tmp/mi_repo
+
+# Copia todos los archivos que se montarán en los volúmenes del contenedor
+sudo cp -r /tmp/mi_repo/Server/* /home/admin
+sudo rm -rf /tmp/mi_repo
+
 sudo systemctl enable docker
 sudo systemctl start docker
 
+# Copia los archivos a carpetas preparadas para montar en los volúmenes
 cd /home/admin
+sudo mkdir otro_vh_html
+sudo cp index.html.en otro_vh_html
+sudo cp index.html.es otro_vh_html
+sudo cp index.var otro_vh_html
+
+sudo mkdir vh_conf
+sudo cp otro-default.conf vh_conf
+sudo cp 000-default.conf vh_conf
+
+sudo mkdir apache2_conf
+sudo cp apache2.conf apache2_conf
+
+
+# Dockerfile
 touch Dockerfile
 
 echo -e "FROM ubuntu:22.04
@@ -22,10 +46,12 @@ RUN apt-get update && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 EXPOSE 80
-VOLUME /var/www/html/otro
-VOLUME /etc/apache2
-VOLUME /etc/apache2/sites-available
+COPY otro_vh_html/ /var/www/html/otro
+COPY apache2_conf/ /etc/apache2
+COPY vh_conf/ /etc/apache2/sites-available
+RUN a2ensite otro-default.conf
 CMD ["apachectl", "-D", "FOREGROUND"]" > Dockerfile
 
+# Monta el contenedor
 sudo docker build -t ubuntu-apache .
 sudo docker run -d -p 8080:80 --name my-apache ubuntu-apache
